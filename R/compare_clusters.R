ee909f76710ab9edc6d3d4e75f261224f5310d5a
María Alonso García <Usuario@DESKTOP-FL7ELLA.intracampus.unileon.es>
2025-01-16 12:55:46 +0100

    Fixing bugs of compare_clusters function

--- R/compare_clusters.R
+++ R/compare_clusters.R
@@ -54,8 +54,8 @@ compare_clusters <- function(cluster_list1, cluster_list2,
   colnames(semantic_similarity) <- paste("Cluster ", 1:n, sep = "")
   for(m in 1:m){
     for(n in 1:n){
-      value <- mgoSim(GO1 = unlist(Perirenal[m]),
-                      GO2 = unlist(Tail[n]),
+      value <- mgoSim(GO1 = unlist(List1[m]),
+                      GO2 = unlist(List2[n]),
                       semData = GOData,
                       measure = method,
                       combine = combine)
