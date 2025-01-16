5d0cd0e114d2ea9153151a93725979cae2af461c
María Alonso García <Usuario@DESKTOP-FL7ELLA.intracampus.unileon.es>
2025-01-16 12:05:25 +0100

    Update visualize_go_hierarchy saving options

--- test/example.R
+++ test/example.R
@@ -43,6 +43,15 @@ visualize_go_hierarchy(go_list1 = GO_BP$ID,
                        verbose = "some", # Just to know more about the process that is ocurring
                        save_plot = F)
 
+visualize_go_hierarchy(go_list1 = GO_BP$ID, nb_lists = "single",
+                       shape1 = "circle", ontology = "BP",
+                       simplification = T,
+                       min_node_size = 1, max_node_size = 10,
+                       layout = "tree", clustering = T, clusters = cluster,
+                       col_palette = colors, legend = T, labs = "GO-Terms",
+                       verbose = "some", save_plot = T,
+                       PNG = "./inst/images/plot1.png")
+
 # Filtering to obtain only nodes of Cluster 5 ----
 filter_and_visualize_cluster(clusters = cluster,
                              selected_cluster = 5,
